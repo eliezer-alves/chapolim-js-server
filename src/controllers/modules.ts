81bf1b980fd1f55dbbe61db7c368e16d2217c34c
Eliezer Cardoso Alves <eliezer.c.alves2015@gmail.com>
2023-01-25 22:33:11 -0300

    feat: handle errors

--- src/controllers/modules.ts
+++ src/controllers/modules.ts
@@ -2,20 +2,41 @@ import { Request, Response } from "express";
 import { Modules } from "@eliezeralves/chapolim-js";
 
 export const create = async (req: Request, res: Response) => {
-  return res.json({ data: req.body });
+  try {
+    const { basePath, name, withViews, withRoutes } = req.body;
+    Modules.create(basePath, name, withRoutes, withViews);
+    return res.json({ data: req.body });
+  } catch (err: any) {
+    return res.json({ error: err.message });
+  }
 };
 
 export const list = async ({ body }: Request, res: Response) => {
-  const data = Modules.list(body.basePath);
-  return res.json(data);
+  try {
+    const data = Modules.list(body.basePath);
+    return res.json(data);
+  } catch (err: any) {
+    return res.json({ error: err.message });
+  }
+
 };
 
 export const show = async ({ body }: Request, res: Response) => {
-  const data = Modules.show(body.basePath, body.name);
-  return res.json(data);
+  try {
+    const data = Modules.show(body.basePath, body.name);
+    return res.json(data);
+  } catch (err: any) {
+    return res.json({ error: err.message });
+  }
+
 };
 
 export const diagram = async ({ body }: Request, res: Response) => {
-  const data = Modules.showDiagram(body.basePath, body.name);
-  return res.json({ data });
+  try {
+    const data = Modules.showDiagram(body.basePath, body.name);
+    return res.json({ data });
+  } catch (err: any) {
+    return res.json({ error: err.message });
+  }
+
 };
